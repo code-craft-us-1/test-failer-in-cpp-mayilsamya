bbd1dafeb022f593b142dc33ba7d102f4f968327
Mayilsamy A <mayilsamy.a@philips.com>
2024-09-19 13:08:40 +0530

    Task updated

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,21 +1,47 @@
 #include <assert.h>
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+using  std::stringstream, std::ios_base, std::string, std::cout, std::vector;
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+const char* majorColor[] = { "White", "Red", "Black", "Yellow", "Violet" };
+const char* minorColor[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+int majorColorsCount = sizeof(majorColor) / sizeof(majorColor[0]);
+int minorColorsCount = sizeof(minorColor) / sizeof(minorColor[0]);
+
+stringstream printColorMap() {
+    stringstream message("\n", ios_base::app | ios_base::out);
     int i = 0, j = 0;
-    for (i = 0; i < 5; i++) {
-        for (j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+    for (i = 0; i < majorColorsCount; i++) {
+        for (j = 0; j < minorColorsCount; j++) {
+            message << i*minorColorsCount+j<< " | " <<majorColor[i]<< " | " <<minorColor[i]<< "\n";
         }
     }
-    return i * j;
+    cout << message.str();
+    return message;
 }
 
 int main() {
-    int result = printColorMap();
-    assert(result == 25);
+    std::string outtext;
+    stringstream result(printColorMap().str());
+    string line;
+    vector<string> stringList;
+    // Loop until the end of the string
+    while (getline(result, line)) {
+        if (!line.empty()) {
+            stringList.push_back(line);
+        }
+    }
+    for (int i = 0; i < majorColorsCount; i++) {
+        for (int j = 0; j < minorColorsCount; j++) {
+            int code = (i * minorColorsCount+ j);
+            string stringLine = stringList.at(code);
+            assert(stringLine.find(std::to_string(code+1)) != string::npos);
+            assert(stringLine.find(majorColor[i]) != string::npos);
+            assert(stringLine.find(minorColor[j]) != string::npos);
+       }
+    }
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
